ea95d4b25198275924f556d831f5650b8dc81627
DasNeuling <idontwantthatcookie@gmail.com>
2016-11-13 00:23:23 +0100

    Implemented neural network for binary decision
    - Learns from either readme or description
    - Tries to seperate dev / not dev
    - Improved utilities
    - Code is probably ugly and inefficient as hell but I just want to
    sleep right now

--- First Try/nn.py
+++ First Try/nn.py
@@ -7,25 +7,42 @@ Created on Tuesday Nov 1 01:59:00 2016
 
 import utilities
 from utilities import *
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 
+def test_sentence(sentence):
+    sentence = process_text(sentence)
+    vec = vectorizer.transform([sentence]).toarray()
+    print "Label", label_names[np.argmax(model.predict_proba(vec))]
+    out = model.predict_proba(vec)[0]
+    prob_dev = out[0]
+    prob_not = out[1]
+    print "Proba DEV: {0:.2f} Not DEV: {1:.2f}".format(prob_dev, prob_not)
 
+# Constants
+binary = True
 
+print "Get and process data"
 # Get raw text + labels
-texts, labels, label_names = get_data()
+features, labels, label_names = get_data('readme', binary=binary)
+features, labels = shuffle_data(features, labels)
+#features = np.matrix(features)
 
+print "Vectorize data"
 # trainingsdaten werden in eingabedaten (vektoren) umgewandelt
-# features ist dann matrix bestehend aus den einzelnen vektoren
-features = vectorize_text(texts)
+# features ist dann matrix bestehend aus den einzelnen vektore
+features, vectorizer = vectorize_text(features, max_features=2000)
 
 # x sind die eingabematrizen, y sind die vektoren in denen die ergebnisse stehen
-x_train, x_test, y_train, y_test = split_train_test(features, labels, ratio=0.7, shuffle=False)
+x_train, x_test, y_train, y_test = split_train_test(features, labels, ratio=0.9, shuffle=True)
 
+print "Prepare one-hot-encoding"
 # One-Hot-Encoding needed for Neural Net Output
 y_train = one_hot_encoding(y_train)
 y_test = one_hot_encoding(y_test)
 
+print "Build model"
 # Struktur des Netzes
 model = Sequential()
 input_size = x_train.shape[1]
@@ -34,18 +51,23 @@ model.add(Activation('relu'))
 model.add(Dense(input_size * 3))
 model.add(Activation('relu'))
 # Output Layer, one neuron per class
-model.add(Dense(7))
+if binary:
+    model.add(Dense(2))
+else:
+    model.add(Dense(8))
 # Softmax zum Normalisieren der Werte, damit Wert des Neurons WSK in % angibt
 model.add(Activation('softmax'))
 
 model.compile(metrics=['accuracy'], optimizer='adam', loss='categorical_crossentropy')
 
+print "Train model"
 # Train for 10 epochs
-model.fit(x_train, y_train, nb_epoch=10, shuffle=True)
+model.fit(x_train, y_train, nb_epoch=5, shuffle=True)
 
 #variablen zum analysieren der ergebnisse
 succ = 0
 total = 0
+dev_pred_count = 0
 dev_count = 0
 
 # Test other samples
@@ -56,11 +78,13 @@ for i in xrange(len(x_test)):
             succ = succ + 1
         total = total + 1
     if np.argmax(pred[i]) == 0:
-        dev_count = dev_count + 1
+        dev_pred_count += 1
+    if np.argmax(y_test[i]) == 0:
+        dev_count += 1
 succ = succ / float(total)
 
-print "Ratio assumed dev/ all classes", float(dev_count) / len(x_test)
-
+print "Ratio assumed dev/ all classes", float(dev_pred_count) / len(x_test)
+print "Ratio dev / all classes", float(dev_count) / len(y_test)
 print "Result without dev:", succ
 acc = model.evaluate(x_test, y_test, verbose=0)
 print "Result with dev: ", acc[1]
--- First Try/utilities.py
+++ First Try/utilities.py
@@ -8,56 +8,162 @@ import random
 import json
 from urllib2 import Request, urlopen, URLError
 import re
+from nltk.stem import PorterStemmer
+import string
+import sklearn
 
+# Constants
+max_stars = 60000 # Max found in data was 52762
+max_forks =  10000 # Max found in data was 9287
+max_watches = 4000 # Max found in data was 3709
+
+stemmer = PorterStemmer()
 
 def one_hot_encoding(labels):
-    # Zur Zeit des Tests war nicht garantiert, dass alle 7 Klassen vertreten waren
-    # One Hot Encoding muss aber für Models immer gleiche Anzahl an Spalten haben
-    arr = np.zeros((len(labels), 7), np.uint8)
+    # Get labels of type [1, 0, 2, ...] and convert to array
+    # of type [[0, 1, 0, ..], [1, 0, 0, ..], [0, 0, 1, ...]]
+    arr = np.zeros((len(labels), max(labels) + 1), np.uint8)
     arr[np.arange(len(labels)), labels] = 1
     return arr
 
-def text_from_base64(text):
-    return base64.b64decode(text)
-
-def process_text(text):
-
-    return text
-
-def shuffle_data(x_train, y_train):
-    x = list(zip(x_train, y_train))
-    random.shuffle(x)
-    x_train, y_train = zip(*x)
-    return x_train, y_train
-
-
-def get_data(whatIWant='description'):
+def get_unlabeled_data(whatIWant='description'):
     # Standardmäßig wird NUR die description verwendet, nicht die readme
 
     #hole data als dict
-    data = api_call()
+    data = api_call(url="Gimme unlabeld pls")
+    
+    features = []
+
+    for i in xrange(len(data)):
+        feature = None
+        if whatIWant == 'readme':
+            #nur die readme ist anscheinend decoded
+            feature = text_from_base64(data[i][whatIWant]).decode('utf-8')
+        elif whatIWant == 'meta':
+            """ 
+            Availible metadata: description, author, url, tree, watches, 
+                                class, languages, tagger, stars, readme, 
+                                forks, id, name
+            """
+            feature = []
+            sample = data[i]
+            feature.append(float(sample['watches']) / max_watches)
+            feature.append(float(sample['stars']) / max_stars)
+            feature.append(float(sample['forks']) / max_forks)
+            features.append(feature)
+        else:
+            feature = data[i][whatIWant]
+        if whatIWant != 'meta':
+            feature = process_text(feature)
+        features.append(feature)
+    return features
+    
+def text_from_base64(text):
+    missing_padding = len(text) % 4
+    if missing_padding != 0:
+        text += b'='* (4 - missing_padding)
+    text = None
+    try:
+        text = base64.b64decode(text)
+    except TypeError:
+        print "Error decoding readme"
+
+    return text
+
+def process_text(text):
+    # Process string
+    readme_codefree = ""
+    words = ""
+    final_words = ""
+    if text is not None:
+        for no_code in text.split("```")[::2]:
+            # skip content code in e.g.  blalba```code```blabla
+            readme_codefree += no_code
+        #for word in readme_codefree.split():
+        # Remove urls
+        readme_urlfree = re.sub(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', ' ', readme_codefree)
+        for word in ((char if char.isalpha() else " ") for char in readme_urlfree):
+            try:
+                words += word.decode('ascii')
+            except UnicodeEncodeError:
+                continue
+        words = " ".join(words.split())
+        for word in words.split():
+            final_words += stemmer.stem(word) + " "
+
+    return words
+
+def shuffle_data(a, b):
+    return sklearn.utils.shuffle(a, b)
+
+
+def get_data(whatIWant='description', binary = False, equal=False):
+    # Standardmäßig wird NUR die description verwendet, nicht die readme
+
+    #hole data als dict
+    data = api_call(equal=equal)
     #liste mit strings von den feature texten
-    texts = []
+    features = []
     #die namen der klassen
     label_names = []
     #die klassen
     labels = []
+
     # vectorizer braucht liste von strings, hier wirds umgewandelt
     for i in xrange(len(data)):
+        feature = None
         if whatIWant == 'readme':
             #nur die readme ist anscheinend decoded
-            text = text_from_base64(data[i][whatIWant])
-        text = data[i][whatIWant]
-        #text = text.replace('\n', ' ')
-        texts.append(text)
-        label = data[i]['class']
+            try:
+                feature = base64.b64decode(data[i][whatIWant])
+            except TypeError:
+                continue
+            feature = feature.decode('utf-8')
+        elif whatIWant == 'meta':
+            """ 
+            Availible metadata: description, author, url, tree, watches, 
+                                class, languages, tagger, stars, readme, 
+                                forks, id, name
+            """
+            feature = []
+            sample = data[i]
+            feature.append(float(sample['watches']) / max_watches)
+            feature.append(float(sample['stars']) / max_stars)
+            feature.append(float(sample['forks']) / max_forks)
+            features.append(feature)
+        else:
+            feature = data[i][whatIWant]
+
+        if whatIWant != 'meta':
+            feature = process_text(feature)
+        if binary:
+            if data[i]['class'] == 'DEV':
+                label = 'DEV'
+            else:
+                label = 'NOTDEV'
+        else:
+            label = data[i]['class']
         if label not in label_names:
             label_names.append(label)
+        
+        features.append(feature)
         labels.append(label_names.index(label))
-    return (texts, labels, label_names)
+        #if i % 50 == 0:
+        #    print "{} repos processed".format(i)
+    return (features, labels, label_names)
+
+def get_batch(features, labels, nb_batch):
+    x, y = shuffle_data(features, labels)
+    return (x[:nb_batch], y[:nb_batch])
 
 
-def api_call(url='http://classifier.leimstaedtner.it/ajax.php?key=api:all'):
+def api_call(equal=False):
+    filter = base64.b64encode(b'id>0')
+    url = None
+    if equal:
+        url = 'http://classifier.leimstaedtner.it/ajax.php?key=api:equal&filter='+filter.decode("utf-8")
+    else:
+        url = 'http://classifier.leimstaedtner.it/ajax.php?key=api:all&filter='+filter.decode("utf-8")
     request = Request(url)
     try:
         response = urlopen(request)
@@ -66,9 +172,9 @@ def api_call(url='http://classifier.leimstaedtner.it/ajax.php?key=api:all'):
         print 'Error with api call', e
     return data
 
-#nimmt die ersten ratio * 100% elemente zum trainieren, rest zum testen
-def split_train_test(features, labels, ratio=0.7, shuffle=True):
-    cut = int(ratio * len(features))
+# Nimmt die ersten ratio * 100% Elemente zum trainieren, Rest zum Testen
+def split_train_test(features, labels, ratio=0.7, shuffle=False):
+    cut = int(ratio * len(labels))
     features_train, labels_train = features[:cut], labels[:cut]
     features_test, labels_test = features[cut:], labels[cut:]
     
@@ -82,15 +188,16 @@ def split_train_test(features, labels, ratio=0.7, shuffle=True):
 # von unwichtigen wörtern
 # https://de.wikipedia.org/wiki/Tf-idf-Maß
 # http://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html
-def vectorize_text(features):
+def vectorize_text(features, max_features=2000):
     vectorizer = TfidfVectorizer(sublinear_tf=True,
                             stop_words='english',
                             decode_error='strict',
                             analyzer='word',
+                            max_features=max_features,
                             max_df=0.5 # Verwendet im ML-Kurs unter Preprocessing                   
                             )
     feature_vec = vectorizer.fit_transform(features)
-    return feature_vec.toarray()
+    return feature_vec.toarray(), vectorizer
 
 # wird im moment nicht verwendet, kann aber später hilfreich sein
 #def get_synonyms(str):
