67250ecebe1858694685dfc1eeea570a40e7b170
Michael Leimstädtner <michi3@hotmail.de>
2016-11-20 20:49:57 +0100

    Working sample + restructure merge

--- Application/Controller/HomeController.py
+++ Application/Controller/HomeController.py
@@ -6,51 +6,51 @@ from bottle import Bottle, route, run, static_file
 
 homebottle = Bottle()
 
-@route('/')
+@homebottle.get('/')
 def home():
-    return static_file("index.html", root="../View/", mimetype='text/html')
+    return static_file("index.html", root="View", mimetype='text/html')
 
-@route('/bootstrap/js/<filename:re:.*\.js>')
+@homebottle.get('/bootstrap/js/<filename:re:.*\.js>')
 def getBS(filename):
-    return static_file(filename, root="../View/bootstrap/js", mimetype='text/javascript')
+    return static_file(filename, root="View/bootstrap/js", mimetype='text/javascript')
 
-@route('/scripts/<filename:re:.*\.js>')
+@homebottle.get('/scripts/<filename:re:.*\.js>')
 def getScript(filename):
-	return static_file(filename, root="../View/scripts", mimetype='text/javascript')
+	return static_file(filename, root="View/scripts", mimetype='text/javascript')
 
-@route('/bootstrap/css/<filename:re:.*\.css>')
+@homebottle.get('/bootstrap/css/<filename:re:.*\.css>')
 def getBSC(filename):
-	return static_file(filename, root="../View/bootstrap/css", mimetype='text/css')
+	return static_file(filename, root="View/bootstrap/css", mimetype='text/css')
 
-@route('/css/<filename:re:.*\.css>')
+@homebottle.get('/css/<filename:re:.*\.css>')
 def getCSS(filename):
-	return static_file(filename, root="../View/css", mimetype='text/css')
+	return static_file(filename, root="View/css", mimetype='text/css')
 
-@route('/fonts/<filename:re:.*\.woff>')
-def getCSS(filename):
-	return static_file(filename, root="../View/fonts", mimetype='application/x-font-woff')
+@homebottle.get('/fonts/<filename:re:.*\.woff>')
+def getf(filename):
+	return static_file(filename, root="View/fonts", mimetype='application/x-font-woff')
 
-@route('/fonts/<filename:re:.*\.woff2>')
-def getCSS(filename):
-	return static_file(filename, root="../View/fonts", mimetype='application/x-font-woff2')
+@homebottle.get('/fonts/<filename:re:.*\.woff2>')
+def getf2(filename):
+	return static_file(filename, root="View/fonts", mimetype='application/x-font-woff2')
 
-@route('/fonts/<filename:re:.*\.ttf>')
-def getCSS(filename):
-	return static_file(filename, root="../View/fonts", mimetype='application/x-font-ttf')
+@homebottle.get('/fonts/<filename:re:.*\.ttf>')
+def getf3(filename):
+	return static_file(filename, root="View/fonts", mimetype='application/x-font-ttf')
 
-@route('/bootstrap/fonts/<filename:re:.*\.woff>')
-def getCSS(filename):
-	return static_file(filename, root="../View/bootstrap/fonts", mimetype='application/x-font-woff')
+@homebottle.get('/bootstrap/fonts/<filename:re:.*\.woff>')
+def getBSf(filename):
+	return static_file(filename, root="View/bootstrap/fonts", mimetype='application/x-font-woff')
 
-@route('/bootstrap/fonts/<filename:re:.*\.woff2>')
-def getCSS(filename):
-	return static_file(filename, root="../View/bootstrap/fonts", mimetype='application/x-font-woff2')
+@homebottle.get('/bootstrap/fonts/<filename:re:.*\.woff2>')
+def getBSf2(filename):
+	return static_file(filename, root="View/bootstrap/fonts", mimetype='application/x-font-woff2')
 
-@route('/bootstrap/fonts/<filename:re:.*\.ttf>')
-def getCSS(filename):
-	return static_file(filename, root="../View/bootstrap/fonts", mimetype='application/x-font-ttf')
+@homebottle.get('/bootstrap/fonts/<filename:re:.*\.ttf>')
+def getBSf3(filename):
+	return static_file(filename, root="View/bootstrap/fonts", mimetype='application/x-font-ttf')
 
-@route('/post/<key>')
+@homebottle.get('/post/<key>')
 def api(key):
 	if (key == "click"):
 		return "Click event called"
