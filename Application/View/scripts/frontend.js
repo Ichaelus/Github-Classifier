a60112fed5ae5420573ccbafe2a277153f830908
Michael Leimstädtner <michi3@hotmail.de>
2016-11-20 22:16:52 +0100

    Good night commit :)

--- Application/View/scripts/frontend.js
+++ Application/View/scripts/frontend.js
@@ -19,7 +19,8 @@ updateVue({modules: [
 			classes: "input",
 			isActive: "",
 			html: '<div class="left notchosen"><h4>Sample Generator</h4><span>With Active Learning</span></div>\
-              <div class="right notchosen"><h4>User Input</h4><span>Specific Repository</span></div>'
+              <div class="right notchosen"><h4>User Input</h4><span>Specific Repository</span></div>',
+              handleData: function() { return this.html}
 		},
 		{
 			title: "Active Learning",
@@ -37,46 +38,67 @@ updateVue({modules: [
     <span>Ionic</span> ... <span>?</span>\
   </li>\
   </ul>\
-  </div>'
+  </div>',
+  handleData: function() { return this.html}
 		},
 		{
 			title: "Sample Extractor",
 			classes: "input",
 			isActive: "active",
-			html: '<div style="text-align: center;">\
-			<img src="http://bestanimations.com/Science/Gears/loadinggears/gear-animation-5.gif">\
-			<!--<img src="https://darkiemindyou.files.wordpress.com/2015/04/loading6_230x230-cooler.gif?w=682" style="position: absolute;margin-left: -89px;width: 60px;margin-top: 115px;">\
-			</div>'
+			html: '<div class="center">	<p class="general_desc">Fetching data for <span class="highlighted">Ionic</span>...</p>		<div class="row"><div class="col-xs-4 col-xs-offset-1">\
+  <div class="gitprev">\
+    <span>github.com/SeraphimSerapis/Ionic/</span>\
+    <div class="gitcontent"><h4>Ionic</h4>\
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br> At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br><br> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </div>\
+  </div>\
+</div><div class="col-xs-6"><img src="http://bestanimations.com/Science/Gears/loadinggears/gear-animation-5.gif"></div>\
+</div>			<!--<img src="https://darkiemindyou.files.wordpress.com/2015/04/loading6_230x230-cooler.gif?w=682" style="position: absolute;margin-left: -89px;width: 60px;margin-top: 115px;">			</div>--></div>',
+handleData: function() { return this.html}
 		},
 		{
 			title: "Feature Vector",
 			classes: "input",
 			isActive: "",
-			html: ''
+			html: '',
+			data: {"api_calls":54,"api_url":"https:\/\/api.github.com\/repos\/ilonaa\/Robotti","author":"ilonaa","avg_commit_length":2,"branch_count":2,"class":"UNLABELED","commit_count":98,"commit_interval_avg":0,"commit_interval_max":0,"contributors_count":0,"description":"","files":"","file_count":0,"folders":"","folder_count":0,"forks":0,"hasDownloads":true,"hasWiki":true,"isFork":false,"open_issues_count":0,"language_main":"","language_array":"","name":"Robotti","readme":"","stars":0,"treeArray":"","treeDepth":0,"url":"https:\/\/github.com\/ilonaa\/Robotti","watches":0},
+			handleData: function(){
+				if(this.html == ""){
+					vstring = "";
+					for (let key in this.data) {
+						vstring +="<div class='row'><div class='col-xs-6'>"+key+"</div><div class='col-xs-6'>"+this.data[key]+"</div></div>"	
+					};
+					this.html = '<p class="general_desc center">Following data has been extracted from <span class="highlighted">Ionic</span>:</p><div class="vector">'+vstring+'</div>';
+				}
+				return this.html;
+			}
 		},
 		{
 			title: "Ensemble Learner",
 			classes: "input",
 			isActive: "",
-			html: ''
+			html: '',
+			handleData: function() { return this.html}
 		},
 		{
 			title: "Probability Diagram",
 			classes: "input",
 			isActive: "",
-			html: ''
+			html: '',
+			handleData: function() { return this.html}
 		},
 		{
 			title: "Result",
 			classes: "input",
 			isActive: "",
-			html: ''
+			html: '',
+			handleData: function() { return this.html}
 		},
 		{
 			title: "New sample pool",
 			classes: "input",
 			isActive: "",
-			html: ''
+			html: '',
+			handleData: function() { return this.html}
 		}
 	]});
 
