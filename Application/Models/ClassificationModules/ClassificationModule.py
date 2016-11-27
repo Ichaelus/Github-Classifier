11b84a44f2d9f36b04692dee10c40b9d1736460a
platingandalf <kessler.martin@gmx.net>
2016-11-28 00:08:25 +0100

    added newDirForModule
    auskommentiert (hab sie noch nicht getestet)

--- Application/Models/ClassificationModules/ClassificationModule.py
+++ Application/Models/ClassificationModules/ClassificationModule.py
@@ -10,6 +10,7 @@ import numpy as np
 
 import xml.etree.ElementTree as ET
 import os.path
+import os.mkdir
 
 class ClassificationModule:
     __metaclass__ = ABCMeta
@@ -228,6 +229,21 @@ class ClassificationModule:
 		data = pickle.load(f)
 		#returned ein ClassificationModule
 		return data
+	
+	"""
+	@classmethod
+	def newDirForModule(self):
+		"""#builds a new directory and xml-file if it doesnt exit
+		"""
+  		tmpPath = os.path.abspath(".") 
+  		tmpPath = os.path.join(tmpPath, self.path)
+  		if os.path.exists(tmpPath) == False:
+    			os.mkdir(tmpPath)
+    			# throws a  OSError if path already exits
 
-
-    
+    			tmpPath = os.path.join(tmpPath, self.name + '.xml')
+    			d = open(tmpPath, "w")
+    			d.write("<data></data>\n")
+    			d.close()
+		return None
+    	"""
