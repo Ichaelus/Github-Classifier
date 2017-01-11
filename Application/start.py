628baac0a90ab5b31d7a5935ad3309287d8bb85c
DasNeuling <idontwantthatcookie@gmail.com>
2017-01-11 17:03:37 +0100

    Added new classifiers. Watch out: Training times are incredibly high.

--- Application/start.py
+++ Application/start.py
@@ -28,8 +28,9 @@ from Models.ClassificationModules.lrstacking import lrstacking
 from Models.ClassificationModules.svmall import svmall
 from Models.ClassificationModules.rfall import allrandomforest
 from Models.ClassificationModules.gbrtmetaonly import gbrtmetaonly
-#from Models.ClassificationModules.gbrtreadmeonly import gbrtreadmeonly
+from Models.ClassificationModules.gbrtreadmeonly import gbrtreadmeonly
 from Models.ClassificationModules.gbrtfilesandfolders import gbrtfilesandfolders
+from Models.ClassificationModules.gbrtdescriptionmeta import gbrtdescriptionmeta
 import Models.DatabaseCommunication as DC
 
 print("Starting application..")
@@ -57,9 +58,10 @@ classifiers['metaonlysvc'] = metaonlysvc()
 classifiers['metaonlyadaboost'] = metaonlyadaboost()
 classifiers['metaonlyrandomforest'] = metaonlyrandomforest()
 classifiers['reponamelstm'] = reponamelstm()
-#classifiers['gbrtreadmeonly'] = gbrtreadmeonly(readmeCorpus)
+classifiers['gbrtreadmeonly'] = gbrtreadmeonly(readmeCorpus)
 classifiers['gbrtfilesandfolders'] = gbrtfilesandfolders(filenameCorpus, foldernameCorpus)
 classifiers['gbrtmetaonly'] = gbrtmetaonly()
+classifiers['gbrtdescriptionmeta'] = gbrtdescriptionmeta(descriptionCorpus)
 
 #classifiers['readmelstm'] = readmelstm()
 
