2db69280d243bdea8eadca1d0e4b18264d90dd28
Un4med <stefangrafberger@googlemail.com>
2017-01-19 21:24:49 +0100

    each class weighted equally for NN getClassWeights

--- Application/Models/ClassificationModules/nnall.py
+++ Application/Models/ClassificationModules/nnall.py
@@ -91,7 +91,7 @@ class nnall(ClassificationModule):
             train_samples.append(formatted_sample)
             train_lables.append(oneHot(getLabelIndex(sample)))
         train_lables = np.asarray(train_lables)
-        train_result = self.model.fit(train_samples, train_lables, nb_epoch=nb_epoch, shuffle=shuffle, verbose=verbose, class_weight=getClassWeights()) #class_weight=getClassWeights()
+        train_result = self.model.fit(train_samples, train_lables, nb_epoch=nb_epoch, shuffle=shuffle, verbose=verbose, class_weight=getClassWeights())
         self.isTrained = True
         return train_result
 
